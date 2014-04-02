497fa6937e3effe0d98ed7751cbe372722f568d4
Jonathan Rivera - Picerne <jrivera@picernefl.com>
2014-04-02 15:10:44 -0400

    Update base and footer

--- base.php
+++ base.php
@@ -2,8 +2,31 @@
 
 <body <?php body_class(); ?>>
 
+<div class="main-wrapper" role="document">
 
+<?php if (is_front_page()){
+	  
+	  //@todo add Slider/Image
+      }
+	  
+	  else{
+		//@todo add full width featured Image  
+	  }
+	  
+?>
 
+  <div class="content row">
+    
+    <main class="main" role="main">
+    
+    </main> <!-- end main element -->
+    
+    <?php //@todo add sidebar conditional option ?>
+  
+  </div>  <!-- end .content .row --> 
+
+</div> <!-- end .main-wrapper -->
+	 
 <?php get_template_part('templates/footer'); ?>
  
 </body>
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
-<footer>
+<footer role="contentinfo">
   <div class="container">
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. Another <a href="http://www.picernerealestategroup.com" target="_blank" rel="nofollow">Picerne</a> Property</p>
   </div>
 </footer>
 
