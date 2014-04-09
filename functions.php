317a1c4da9886e4dd9d20114c448c23bcce3ce37
Jonathan Rivera - Picerne <jrivera@picernefl.com>
2014-04-09 17:50:28 -0400

    setup achilles customizer for logo upload

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@
 require_once locate_template ('lib/achilles-settings.php'); 
 require_once locate_template ('lib/theme-wrapper.php');
 require_once locate_template ('lib/theme-activation.php');
-//require_once locate_template ('lib/achilles-sidebar');
-//require_once locate_template ('lib/achilles-config');
-//require_once locate_template ('lib/achilles-cleanup.php');
-
+require_once locate_template ('lib/achilles-sidebar.php');			// Sidebar Class
+require_once locate_template ('lib/achilles-config.php');
+require_once locate_template ('lib/achilles-cleanup.php');
+require_once locate_template ('lib/achilles-customizer.php');		//Achilles Theme Customizer Class
 ?>
--- lib/achilles-customizer.php
+++ lib/achilles-customizer.php
@@ -1,6 +1,42 @@
 <?php 
 /**
- * Integrate Theme Customizer into Achilles. 
+ * Class that integrates the WP Theme Customizer into Achilles.
+ *
+ * @link http://codex.wordpress.org/Theme_Customization_API
+ * @since Achilles 1.0
  */
+class Achilles_Customize {
+	/*
+	* Hooks into the customize_register and allows you to add new sections and controls to the Theme Customize screen
+	*/
+	public static function achilles_customize_register( $wp_customize ){
+		
+    //add a section to customize the logo on the website
+    $wp_customize->add_section('achilles_logo', array(
+        'title'    => __('Logo', 'themename'),
+        'priority' => 01,
+    ));
+ 
+     //  =============================
+    //  = Image Upload              =
+    //  =============================
+    $wp_customize->add_setting('achilles_theme_options[achilles_upload_logo]', array(
+        'default'           => 'image.jpg',  
+        'capability'        => 'edit_theme_options',
+        'type'				   => 'option',
+ 
+    ));
+ 
+    $wp_customize->add_control( new WP_Customize_Image_Control($wp_customize, 'achilles_upload_logo', array(
+        'label'    => __('Upload Logo', 'achilles'),
+        'section'  => 'achilles_logo',
+        'settings' => 'achilles_theme_options[achilles_upload_logo]',
+    )));
+	}
+	
 
+}
+
+// Setup the Theme Customizer settings and controls
+add_action( 'customize_register' , array( 'Achilles_Customize' , 'achilles_customize_register' ) );
 ?>
